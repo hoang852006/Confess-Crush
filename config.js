f108c5a382b18a8279aaa1dbea68da28606d37a4
hoang852006 <97816550+hoang852006@users.noreply.github.com>
2022-01-16 12:23:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,18 @@
 const CONFIG = {
-    titleWeb: "gửi....",
-    introTitle: 'bảo khanh',
-    introDesc: `cẩu khanh`,
+    titleWeb: "lời tỏ tình ngọt ngào",
+    introTitle: 'hải an loli',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'chó',
-    desc: 'cẩu ',
-    btnYes: 't ngu',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
+    btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao m lại ngu thế',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    messDesc: 'Tối nay 7h, lq',
+    messDesc: 'vậy thì nhắn cho mình đi',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://facebook.com/hoang.d.ng.98
 }
